3ae96681f4e6c1a360eb43e06220088cbafc014d
jam <wynesine@protonmail.com>
2023-05-12 09:42:36 +0700

    bah

---
+++ static/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    document.querySelector(".proj1but").onClick = function () {
+        open("http://localhost:5500/11/pokedex-project");
+    }
+})
