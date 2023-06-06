96012d17fba7bbcd6851b250ead357bf6931f135
jam <wynesine@protonmail.com>
2023-06-06 14:57:55 +0700

    BEwahaha

--- static/script.js
+++ static/script.js
@@ -1,8 +1,17 @@
-function myFunction() {
-    var x = document.getElementById('projects');
-    if(x.style.display=="block"){
-        x.style.display="none"
-      } else {
-        x.style.display="block"
-      }
-  } 
+// function myFunction() {
+//     var x = document.getElementById('projects');
+//     if(x.style.height=="0px"){
+//         x.style.height="100%"
+//       } else {
+//         x.style.height="0px"
+//       }
+//   } 
+
+//   function myFunction2() {
+//     var x = document.getElementById('contacts');
+//     if(x.style.display=="block"){
+//         x.style.display="none"
+//       } else {
+//         x.style.display="block"
+//       }
+//   } 
